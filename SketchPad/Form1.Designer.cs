e9caa2b624bd22fb752baa46c0cb138f868cfe83
jnttackie009 <jnttackie009@st.ug.edu.gh>
2024-07-09 22:11:56 +0000

    Uploaded the sketchpad file

---
+++ SketchPad/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SketchPad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Canvas = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Canvas.Location = new System.Drawing.Point(0, 0);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(962, 494);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 494);
+            this.Controls.Add(this.Canvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        private void Canvas_Paint(object sender, PaintEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Canvas;
+    }
+}
+
---
+++ SketchPad/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SketchPad
+{
+    public partial class Form1 : Form
+    {
+        private Point startPoint;
+        private Point endPoint;
+        private bool drawing = false;
+        private Pen drawPen = new Pen(Color.Black, 2);
+        private Bitmap drawingBitmap;
+        private Graphics drawingGraphics;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDrawing();
+        }
+
+        private void InitializeDrawing()
+        {
+          
+            Canvas.BackColor = Color.White; 
+            Canvas.BorderStyle = BorderStyle.FixedSingle; 
+            drawingGraphics = Canvas.CreateGraphics(); 
+
+            Canvas.Paint += DrawingPanel_Paint;
+            Canvas.MouseDown += DrawingPanel_MouseDown;
+            Canvas.MouseMove += DrawingPanel_MouseMove;
+            Canvas.MouseUp += DrawingPanel_MouseUp;
+        }
+
+        private void DrawingPanel_Paint(object sender, PaintEventArgs e)
+        {
+         
+        }
+
+        private void DrawingPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            startPoint = e.Location;
+            drawing = true;
+        }
+
+        private void DrawingPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                endPoint = e.Location;
+                drawingGraphics.DrawLine(drawPen, startPoint, endPoint);
+                startPoint = endPoint; 
+            }
+        }
+
+        private void DrawingPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            drawing = false;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+           
+            drawingGraphics.Clear(Canvas.BackColor); 
+        }
+        
+    }
+}
