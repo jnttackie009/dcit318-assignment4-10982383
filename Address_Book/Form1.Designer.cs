ef90e8fc5934a1a18ffb43b2a8ee455483d8ff76
jnttackie009 <jnttackie009@st.ug.edu.gh>
2024-07-09 11:50:25 +0000

    Uploaded the address book file

---
+++ Address_Book/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Address_Book
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.name_textBox = new System.Windows.Forms.TextBox();
+            this.email_textBox = new System.Windows.Forms.TextBox();
+            this.phone_textBox = new System.Windows.Forms.TextBox();
+            this.name_label = new System.Windows.Forms.Label();
+            this.email_label = new System.Windows.Forms.Label();
+            this.phone_number_label = new System.Windows.Forms.Label();
+            this.Title = new System.Windows.Forms.Label();
+            this.save_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // name_textBox
+            // 
+            this.name_textBox.BackColor = System.Drawing.SystemColors.Info;
+            this.name_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name_textBox.Location = new System.Drawing.Point(399, 115);
+            this.name_textBox.Name = "name_textBox";
+            this.name_textBox.Size = new System.Drawing.Size(186, 27);
+            this.name_textBox.TabIndex = 0;
+            // 
+            // email_textBox
+            // 
+            this.email_textBox.BackColor = System.Drawing.SystemColors.Info;
+            this.email_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email_textBox.Location = new System.Drawing.Point(399, 163);
+            this.email_textBox.Name = "email_textBox";
+            this.email_textBox.Size = new System.Drawing.Size(186, 27);
+            this.email_textBox.TabIndex = 1;
+            // 
+            // phone_textBox
+            // 
+            this.phone_textBox.BackColor = System.Drawing.SystemColors.Info;
+            this.phone_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phone_textBox.Location = new System.Drawing.Point(397, 211);
+            this.phone_textBox.Name = "phone_textBox";
+            this.phone_textBox.Size = new System.Drawing.Size(186, 27);
+            this.phone_textBox.TabIndex = 2;
+            // 
+            // name_label
+            // 
+            this.name_label.AutoSize = true;
+            this.name_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name_label.Location = new System.Drawing.Point(322, 114);
+            this.name_label.Name = "name_label";
+            this.name_label.Size = new System.Drawing.Size(70, 25);
+            this.name_label.TabIndex = 3;
+            this.name_label.Text = "Name:";
+            // 
+            // email_label
+            // 
+            this.email_label.AutoSize = true;
+            this.email_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email_label.Location = new System.Drawing.Point(322, 163);
+            this.email_label.Name = "email_label";
+            this.email_label.Size = new System.Drawing.Size(66, 25);
+            this.email_label.TabIndex = 4;
+            this.email_label.Text = "Email:";
+            // 
+            // phone_number_label
+            // 
+            this.phone_number_label.AutoSize = true;
+            this.phone_number_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phone_number_label.Location = new System.Drawing.Point(316, 211);
+            this.phone_number_label.Name = "phone_number_label";
+            this.phone_number_label.Size = new System.Drawing.Size(75, 25);
+            this.phone_number_label.TabIndex = 5;
+            this.phone_number_label.Text = "Phone:";
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(338, 39);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(229, 39);
+            this.Title.TabIndex = 6;
+            this.Title.Text = "Address Book";
+            this.Title.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // save_button
+            // 
+            this.save_button.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.save_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.save_button.Location = new System.Drawing.Point(399, 258);
+            this.save_button.Name = "save_button";
+            this.save_button.Size = new System.Drawing.Size(103, 31);
+            this.save_button.TabIndex = 7;
+            this.save_button.Text = "Save";
+            this.save_button.UseVisualStyleBackColor = false;
+            this.save_button.Click += new System.EventHandler(this.save_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(994, 520);
+            this.Controls.Add(this.save_button);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.phone_number_label);
+            this.Controls.Add(this.email_label);
+            this.Controls.Add(this.name_label);
+            this.Controls.Add(this.phone_textBox);
+            this.Controls.Add(this.email_textBox);
+            this.Controls.Add(this.name_textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox name_textBox;
+        private System.Windows.Forms.TextBox email_textBox;
+        private System.Windows.Forms.TextBox phone_textBox;
+        private System.Windows.Forms.Label name_label;
+        private System.Windows.Forms.Label email_label;
+        private System.Windows.Forms.Label phone_number_label;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Button save_button;
+    }
+}
+
---
+++ Address_Book/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Address_Book
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void save_button_Click(object sender, EventArgs e)
+        {
+            String name = name_textBox.Text;
+            String email = email_textBox.Text;
+            String phone = phone_textBox.Text;
+
+            String filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "details.txt");
+
+            if (name.Equals("") || email.Equals("") || phone.Equals("")) {
+                MessageBox.Show("Please make sure you have provided the requested information", "Empty data");
+            }
+            else
+            {
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(filePath, true))
+                    {
+                        writer.WriteLine($"Name: {name}");
+                        writer.WriteLine($"Email: {email}");
+                        writer.WriteLine($"Phone: {phone}");
+                        writer.WriteLine("--------------------");
+                    }
+
+                    MessageBox.Show("Information saved to file (details) in the same directory.", "Success");
+
+                    name_textBox.Clear();
+                    email_textBox.Clear();
+                    phone_textBox.Clear();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"There was an error while saving the information: {ex.Message}", "Error");
+                }
+            }
+        }
+    }
+}
