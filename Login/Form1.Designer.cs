168be6e8d645cc5bb9c6d3056f114cae487a001d
jnttackie009 <jnttackie009@st.ug.edu.gh>
2024-07-09 11:48:41 +0000

    Uploaded the login file page

---
+++ Login/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.username = new System.Windows.Forms.TextBox();
+            this.username_label = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.MaskedTextBox();
+            this.password_label = new System.Windows.Forms.Label();
+            this.login_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // username
+            // 
+            this.username.BackColor = System.Drawing.SystemColors.Info;
+            this.username.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username.Location = new System.Drawing.Point(344, 130);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(252, 24);
+            this.username.TabIndex = 0;
+            this.username.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // username_label
+            // 
+            this.username_label.AutoSize = true;
+            this.username_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username_label.Location = new System.Drawing.Point(344, 104);
+            this.username_label.Name = "username_label";
+            this.username_label.Size = new System.Drawing.Size(146, 18);
+            this.username_label.TabIndex = 1;
+            this.username_label.Text = "Enter your username";
+            // 
+            // password
+            // 
+            this.password.BackColor = System.Drawing.SystemColors.Info;
+            this.password.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password.Location = new System.Drawing.Point(344, 207);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(252, 24);
+            this.password.TabIndex = 2;
+            this.password.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.password_MaskInputRejected);
+            // 
+            // password_label
+            // 
+            this.password_label.AutoSize = true;
+            this.password_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password_label.Location = new System.Drawing.Point(345, 185);
+            this.password_label.Name = "password_label";
+            this.password_label.Size = new System.Drawing.Size(145, 18);
+            this.password_label.TabIndex = 3;
+            this.password_label.Text = "Enter your password";
+            // 
+            // login_button
+            // 
+            this.login_button.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.login_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_button.Location = new System.Drawing.Point(344, 251);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(252, 35);
+            this.login_button.TabIndex = 4;
+            this.login_button.Text = "Login";
+            this.login_button.UseVisualStyleBackColor = false;
+            this.login_button.Click += new System.EventHandler(this.login_button_clicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(960, 510);
+            this.Controls.Add(this.login_button);
+            this.Controls.Add(this.password_label);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username_label);
+            this.Controls.Add(this.username);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Label username_label;
+        private System.Windows.Forms.MaskedTextBox password;
+        private System.Windows.Forms.Label password_label;
+        private System.Windows.Forms.Button login_button;
+    }
+}
+
---
+++ Login/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void password_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void login_button_clicked(object sender, EventArgs e)
+        {
+            String uname = username.Text;
+            String pwd = password.Text;
+            MessageBox.Show($"Your username is: {uname}\n Your password is: {pwd}");
+        }
+    }
+}
